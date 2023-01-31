0a0be8fd5b78ed1191fb6880c097716a35d7c3cb
blue-beaker <yyy5566778899@126.com>
2023-01-31 22:10:07 +0800

    capture mouse and keyboard input

--- web/orangekvm.js
+++ web/orangekvm.js
@@ -7,7 +7,6 @@ var worker
 var mouseMoveReady=true
 var mouseMode=0
 var debug=0
-var debugstr=""
 function linkws(addr) {
     if(!"Websocket" in window){
         alert("Your browser doesn't support websocket. Please use a browser with websocket support.")
@@ -16,31 +15,38 @@ function linkws(addr) {
     ws = new WebSocket(addr);
     ws.onmessage = function (messageEvent) {
         var message=messageEvent.data;
+        document.getElementById("debugoutput2").innerHTML="<- "+message;
         if (debug) console.log("<- "+message);
         if(message.startsWith("keyPressed ")){
             onKeyPressed(message.slice(start=10));
         }
+        if(message.startsWith("info")){
+            onInfo(message.slice(start=4));
+        }
     }
     ws.onopen = function () {
         wslinked=true
-        document.getElementById("wsstatus").style.setProperty("color","lime")
+        document.getElementById("wsstatus").classList.add("lightlit")
         setTimeout("getPressed()",2000);
     }
     ws.onclose = function () {
         wslinked=false
-        document.getElementById("wsstatus").style.setProperty("color","red")
+        document.getElementById("wsstatus").classList.remove("lightlit")
     }
 }
 function wssend(message){
     if(wslinked){
     ws.send(message);
-    debugstr="-> "+message;
+    document.getElementById("debugoutput").innerHTML="-> "+message;
     if(debug) console.log("-> "+message);}
     else
     reconnect();
 }
 function pressKey(key,press=2) {
     wssend("pressKey "+key+" "+press);
+    // if(key=="caps" || key=="numlock" || key=="scrolllock"){
+    //     setTimeout(getInfo(),500)
+    // }
 }
 function mouseAbsolute(x,y,wheel=0) {
     wssend("mouseAbsolute "+x+" "+y+" "+wheel);
@@ -56,6 +62,7 @@ function mousePressButtons(buttons) {
 }
 function releaseAll() {
     wssend("releaseAll");
+    // setTimeout(getInfo(),500);
 }
 function getPressed() {
     wssend("getPressed");
@@ -70,27 +77,35 @@ function onKeyPressed(message=""){
     var keyboard = document.getElementById("keyboard");
     var buttons=keyboard.getElementsByTagName("button");
     for (i in buttons) {
-        // var button2 = document.getElementById("keyboard_"+pressedKeys[i]);
         var button=buttons[i]
         if(button.id && button.id.startsWith("keyboard_")){
             var name=button.id.replace("keyboard_","")
             if(pressedKeys.includes(name)){
-                // button.style.backgroundColor="#5599FF";
-                // button.style.color="#FFFFFF";
                 button.classList.add("activekey")
             }else{
-                // button.style.backgroundColor="#F0F0F0";
-                // button.style.color="#000000";
                 button.classList.remove("activekey")
             }
         }
     }
 }
-function onPressKey(event=new TouchEvent()){
+function onInfo(message=""){
+    var locks=message.split(" ")[0].replace("locks=","")
+    var num=document.getElementById("lednum")
+    var caps=document.getElementById("ledcaps")
+    var scr=document.getElementById("ledscroll")
+    if(locks[2]=="1") num.classList.add("lightlit")
+    else num.classList.remove("lightlit")
+    if(locks[1]=="1") num.classList.add("lightlit")
+    else caps.classList.remove("lightlit")
+    if(locks[0]=="1") num.classList.add("lightlit")
+    else scr.classList.remove("lightlit")
+
+}
+function onTouchKey(event=new TouchEvent()){
     if(event.type.startsWith("touch")) event.preventDefault()
     if(event.target.id.startsWith("keyboard_")){
         var key=event.target.id.replace("keyboard_","")
-        console.log(key)
+        if(debug)console.log(key)
         if(event.type=="touchstart" || event.type=="mousedown") {
             pressKey(key,1)
         }
@@ -99,9 +114,60 @@ function onPressKey(event=new TouchEvent()){
         }
     }
 }
+keys={
+    "Escape":"esc",
+    "Backquote":"`",
+    "Minus":"-",
+    "Equal":"=",
+    "BracketLeft":"[",
+    "BracketRight":"]",
+    "Backslash":"\\",
+    "Semicolon":";",
+    "Quote":"'",
+    "Comma":",",
+    "Period":".",
+    "Slash":"/",
+    "CapsLock":"caps",
+    "ShiftLeft":"lshift",
+    "ShiftRight":"rshift",
+    "ControlLeft":"lctrl",
+    "ControlRight":"rctrl",
+    "AltLeft":"lalt",
+    "AltRight":"ralt",
+    "MetaLeft":"lsuper",
+    "MetaRight":"rsuper",
+    "ArrowUp":"up",
+    "ArrowDown":"down",
+    "ArrowLeft":"left",
+    "ArrowRight":"right",
+    "NumpadMultiply":"numpad*",
+    "NumpadDivide":"numpad/",
+    "NumpadSubtract":"numpad-",
+    "NumpadAdd":"numpad+",
+    "NumpadDecimal":"numpad.",
+}
+function onKey(event=new KeyboardEvent()){
+    event.preventDefault()
+    var key=event.code
+    if(key in keys){
+        key=keys[key]
+    }else if(key.startsWith("Digit")){
+        key=key.substring(5)
+    }else if(key.startsWith("Key")){
+        key=key.substring(3).toLowerCase()
+    }else{
+        key=key.toLowerCase()
+    }
+    if(debug) console.log(event.code,key)
+    if (event.type=="keydown"){
+        pressKey(key,1)
+    }
+    if (event.type=="keyup"){
+        pressKey(key,0)
+    }
+}
 function onWheel(event=new WheelEvent()){
     event.preventDefault()
-    debugstr=event.deltaX+","+event.deltaY+","+event.deltaZ+","
     if(event.deltaY>0) mouseRelative(0,0,-1)
     if(event.deltaY<0) mouseRelative(0,0,1)
 }
@@ -217,7 +283,7 @@ function mouseButtonsListeners(){
 }
 
 function loadImage(){
-    select=document.getElementById("selectimage")
+    var select=document.getElementById("selectimage")
     wssend("imageload "+select.options[select.selectedIndex].value)
 }
 function unloadImage(){
@@ -231,25 +297,72 @@ function addListeners(){
         btn=buttons[i];
         if(btn.addEventListener){
             btn.onclick=null
-            btn.addEventListener("touchstart",onPressKey);
-            btn.addEventListener("touchend",onPressKey);
-            btn.addEventListener("touchcancel",onPressKey);
-            btn.addEventListener("mousedown",onPressKey);
-            btn.addEventListener("mouseup",onPressKey);
+            btn.addEventListener("touchstart",onTouchKey);
+            btn.addEventListener("touchend",onTouchKey);
+            btn.addEventListener("touchcancel",onTouchKey);
+            btn.addEventListener("mousedown",onTouchKey);
+            btn.addEventListener("mouseup",onTouchKey);
             btn.addEventListener("click",function(event){event.preventDefault()});
         }
     }
+    mouseButtonsListeners();
+    document.addEventListener("pointerlockchange",function(event){
+        if(document.pointerLockElement){
+            startCaptureMouse();
+            startCaptureKeyboard();
+        }else{
+            stopCaptureMouse();
+            stopCaptureKeyboard();
+        }
+    })
+    var stream = document.getElementById("stream")
+    stream.addEventListener("mousedown",function(event) {
+        if(mouseMode==0){
+            stream.requestPointerLock();
+        }else{
+            startCaptureMouse();
+            startCaptureKeyboard();
+        }
+    })
+    stream.addEventListener("touchstart",function(event) {
+        startCaptureKeyboard();
+    })
+}
+function startCaptureMouse(){
     var stream = document.getElementById("stream")
     stream.addEventListener("mousedown",onMouseDrag);
     stream.addEventListener("mouseup",onMouseDrag);
     stream.addEventListener("mousemove",onMouseDrag);
     stream.addEventListener("wheel",onWheel);
     stream.addEventListener("click",function(event){event.preventDefault()});
+    document.getElementById("streamcontainer").style.setProperty("background-color","#83ff83")
+}
+function startCaptureKeyboard(){
+    var stream = document.getElementById("stream")
     stream.addEventListener("touchstart",onTouchDrag);
     stream.addEventListener("touchmove",onTouchDrag);
     stream.addEventListener("touchend",onTouchDrag);
     stream.addEventListener("touchcancel",onTouchDrag);
-    mouseButtonsListeners();
+    document.addEventListener("keydown",onKey);
+    document.addEventListener("keyup",onKey);
+}
+function stopCaptureMouse() {
+    var stream = document.getElementById("stream")
+    stream.removeEventListener("mousedown",onMouseDrag);
+    stream.removeEventListener("mouseup",onMouseDrag);
+    stream.removeEventListener("mousemove",onMouseDrag);
+    stream.removeEventListener("wheel",onWheel);
+    stream.removeEventListener("click",function(event){event.preventDefault()});
+    document.getElementById("streamcontainer").style.setProperty("background-color","#b3b3b3")
+}
+function stopCaptureKeyboard(){
+    var stream = document.getElementById("stream")
+    stream.removeEventListener("touchstart",onTouchDrag);
+    stream.removeEventListener("touchmove",onTouchDrag);
+    stream.removeEventListener("touchend",onTouchDrag);
+    stream.removeEventListener("touchcancel",onTouchDrag);
+    document.removeEventListener("keydown",onKey);
+    document.removeEventListener("keyup",onKey);
 }
 function onLoadComplete(){''
     onResize();
@@ -259,7 +372,4 @@ function onLoadComplete(){''
         mouseMoveReady=true
         if(mouseRelativeX!=0 && mouseRelativeY!=0) mouseRelOffset(0,0)
     },100)
-    setInterval(function() {
-        document.getElementById("debugoutput").innerHTML=debugstr;
-    },15)
 }
