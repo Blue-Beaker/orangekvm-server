bc0d635a1608dd5af03f656454b426fa61ce64ef
blue-beaker <yyy5566778899@126.com>
2023-07-13 00:05:13 +0800

    setgpios on load

--- setgpios.sh
+++ setgpios.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 # This is an example if your board needs some pull-up or pull-downs for CH9329 chip to work.
-echo 11 > /sys/class/gpio/export
-echo 0 > /sys/class/gpio/gpio11/active_low
-echo out > /sys/class/gpio/gpio11/direction
-echo 12 > /sys/class/gpio/export
-echo 1 > /sys/class/gpio/gpio12/active_low
-echo out > /sys/class/gpio/gpio12/direction
+echo 2 > /sys/class/gpio/export
+echo 0 > /sys/class/gpio/gpio2/active_low
+echo out > /sys/class/gpio/gpio2/direction
+echo 3 > /sys/class/gpio/export
+echo 1 > /sys/class/gpio/gpio3/active_low
+echo out > /sys/class/gpio/gpio3/direction
